98851b8fa9da459427ecb121f1b5f56509331ac2
Игорь Малеваный <gethwalk@gmail.com>
2016-01-21 19:52:10 +0200

    Initial Commit

---
+++ QR Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QR Reader
+//
+//  Created by Игорь on 21.01.16.
+//  Copyright © 2016 Ihor Malovanyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
